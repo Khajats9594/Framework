13149248922bc693d8c7f1c097f5220ea2787c94
khajats <khajats@testvagrant.com>
2023-09-15 10:19:31 +0530

    testing the log4j2 configuration

--- src/test/java/org/example/test/Sample.java
+++ src/test/java/org/example/test/Sample.java
@@ -1,21 +1,26 @@
 package org.example.test;
 
 
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.Test;
 import web.config.FrameworkConfig;
 import web.config.factory.ConfigFactory;
-import web.enums.ConfigProperties;
-import web.utils.PropertyUtils;
 
 public class Sample {
 
+    private static final Logger log = LoggerFactory.getLogger(Sample.class);
     @Test
-    public static void test() {
+    public  void test() {
         //Driver.initDriverForWeb();
+        log.error("Testing the config file properties");
+        log.info("Testing the config file properties");
+        log.debug("Testing the config file properties");
         FrameworkConfig config = ConfigFactory.getConfig();
-        System.out.println(config.webUrl());
-//        String url = PropertyUtils.get(ConfigProperties.RUNMODEBROWSER);
+        String url1 = config.webUrl();
+        System.out.println(url1);
+        log.warn("URl used {} :",url1);
+//        String url = PropertyUtils.get(ConfigProperties.BROWSER);
 //        System.out.println(url);
 
     }
