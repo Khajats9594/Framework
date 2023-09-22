04b0dd8978a98f5bf42cf8248dec3c1a0cf673aa
khajats <khajats@testvagrant.com>
2023-09-22 15:57:24 +0530

    refactoring the code

--- src/test/java/org/example/test/Sample.java
+++ src/test/java/org/example/test/Sample.java
@@ -1,6 +1,5 @@
 package org.example.test;
 
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.annotations.Test;
@@ -13,9 +12,9 @@ public class Sample {
     @Test
     public  void test() {
         //Driver.initDriverForWeb();
-        log.error("Testing the config file properties");
-        log.info("Testing the config file properties");
-        log.debug("Testing the config file properties");
+//        log.error("Testing the config file properties");
+//        log.info("Testing the config file properties");
+//        log.debug("Testing the config file properties");
         FrameworkConfig config = ConfigFactory.getConfig();
         String url1 = config.webUrl();
         System.out.println(url1);
